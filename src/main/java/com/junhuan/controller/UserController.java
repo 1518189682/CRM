217d9b300df899c87793b27152376c92b9e84257
lly <liuliyuan666@aliyun.com>
2019-04-15 17:26:35 +0800

    update

--- src/main/java/com/junhuan/controller/UserController.java
+++ src/main/java/com/junhuan/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.junhuan.service.UserService;
-
+//lly
 @Controller
 public class UserController {
 	@Autowired
